10e223629ddfeeb11aaeda073d32e964e4b2c6d2
nhwnahid007 <nhwnahid@gmail.com>
2024-06-05 15:03:03 +0600

    put user data to the database

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 //mongodb
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, Timestamp } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omy4kgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('medvantage').collection('users')
+
+
+app.get('/users', async (req,res)=>{
+    const result = await userCollection.find().toArray()
+    res.send(result)
+})
+
+app.put('/users', async(req,res)=>{
+    const user = req.body;
+    const query = {email: user?.email}
+    //check if the already exist in the database
+    const isExist = await userCollection.findOne(query)
+    if(isExist) {
+        return res.send({message: 'User aleready exist'})
+    }
+    const options = {upsert: true}
+    const updateDoc = {
+        $set: {
+            ...user,
+            Timestamp: Date.now(),
+        }
+    }
+    const result = await userCollection.updateOne(query,updateDoc,options)
+    res.send(result)
+})
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
